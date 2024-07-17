fbadc0674e554bfcc81ecf7487310029721a226b
ujjawalkumarsingh <singhujjawal9793@gmail.com>
2024-07-17 22:01:43 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log(process.env.MONGO);
 
 mongoose.connect(process.env.MONGO)
     .then(()=>{
