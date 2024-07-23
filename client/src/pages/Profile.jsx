7a9d5276d4c3864381c8acc07d853be70b16c550
ujjawalkumarsingh <singhujjawal9793@gmail.com>
2024-07-23 12:17:02 +0530

    compl

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import {useSelector} from 'react-redux'
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user)
   return (
+    
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
