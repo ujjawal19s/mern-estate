ef9e1424114af7e6dc562863f7918defc9530fa4
ujjawalkumarsingh <singhujjawal9793@gmail.com>
2024-07-24 15:04:07 +0530

    sign out user functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,9 +3,10 @@ import { signup } from '../controllers/auth.controller.js';
 import { signin } from '../controllers/auth.controller.js';
 const router = express.Router();
 import { google } from '../controllers/auth.controller.js';
+import { signout } from '../controllers/auth.controller.js';
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google);
-
+router.get('/signout', signout);
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import { app } from '../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure,
   deleteUserStart,
-  deleteUserSuccess,} from '../redux/user/userSlice';
+  deleteUserSuccess, signOutUserFailure, signOutUserSuccess, signOutUserStart} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -55,7 +55,7 @@ export default function Profile() {
   const handleChange = (e)=>{
     setFormData({...formData, [e.target.id]: e.target.value});
   };
-  
+
   // updating the user
   const handleSubmit = async (e)=>{
    e.preventDefault();
@@ -97,6 +97,22 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  const handleSignOut = async ()=>{
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  }
+  
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -145,7 +161,7 @@ export default function Profile() {
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser}
         className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>
