b70a35200beed7ccf7c6b1a2fcaf8f9a4fd2fb82
ujjawalkumarsingh <singhujjawal9793@gmail.com>
2024-07-24 15:32:06 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailur
   deleteUserStart,
   deleteUserSuccess, signOutUserFailure, signOutUserSuccess, signOutUserStart} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -155,6 +156,10 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
 
       </form>
 
